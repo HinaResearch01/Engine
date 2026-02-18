1533a2ce880132734e51397e545e5aff52d8f4be
Tsumita-Hinata0219 <k022g0114@g.neec.ac.jp>
2026-02-18 11:07:50 +0900

    directionallightは実装おｋ

--- Project/Game/Test/Actor/SpotLight/TestSpotLight.cpp
+++ Project/Game/Test/Actor/SpotLight/TestSpotLight.cpp
@@ -5,4 +5,9 @@ void TestSpotLight::Init()
 {
 	IActor::name_ = "TestSpotLight";
 	IActor::AddComp<TestSpotLightComp>();
+
+	auto* tr = IActor::GetComponent<Tsumi::Framework::TransformComponent>();
+	// 上から下を照らす
+	tr->srt.translate= { 0.0f, 10.0f, 0.0f };
+	tr->srt.rotate = { -90.0f, 0.0f, 0.0f };
 }
